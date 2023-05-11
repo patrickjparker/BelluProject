b1b1ac584860f52f10a94c952da3555080ed56e6
patrickjparker <patrickjpark@gmail.com>
2023-05-11 16:03:25 -0600

    Set up tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Survey/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Survey from './components/Survey.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Survey
   }
 }
 </script>
---
+++ src/components/Survey.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex p-2">Hello</div>
+</template>
+
+<script>
+export default {
+    name: 'Survey',
+    data: function() {
+        return {
+            currentStep: 0,
+            STEPS: {
+
+            },
+            response: {
+
+            }
+        }
+    }
+}
+</script>
